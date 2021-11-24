81fbffa91077943f2de9a83d993a6fddbd14faa3
= <=>
2021-11-24 20:45:15 +0700

    [updates] LIGHT_HOUSE public funcs

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -3,14 +3,31 @@
 // Theme module
 //
 
-'use strict';
+"use strict";
 
 // count input
 
-(function () {
-  var countInputs = document.getElementsByClassName('count-input');
+var LIGHT_HOUSE = (function () {
+  var _public = {};
 
-  if (countInputs.length > 0) {
+  // Functions
+  function init() {
+    refresh();
+    return true;
+  }
+
+  function refresh() {
+    var countInputs = document.getElementsByClassName("count-input");
+
+    if (countInputs.length > 0) {
+      refreshCountInputs(countInputs);
+
+      return true;
+    }
+    return false;
+  }
+
+  function refreshCountInputs(countInputs) {
     for (let i = 0; i < countInputs.length; i++) {
       const inputWrap = countInputs[i];
       const buttonDesc = $(inputWrap).find("[data-toggle='count']:first-child");
@@ -23,15 +40,22 @@
         let _nextVal = Math.max(parseInt(currentVal) - 1, 0);
         currentVal = _nextVal;
         inp.val(_nextVal);
-      })
+      });
 
       buttonInc.on("click", function (event) {
         event.preventDefault();
         let _nextVal = parseInt(currentVal) + 1;
         currentVal = _nextVal;
         inp.val(_nextVal);
-      })
-
+      });
     }
+    return countInputs;
   }
+
+  init();
+
+  _public.init = init;
+  _public.refresh = refresh;
+
+  return _public;
 })();
